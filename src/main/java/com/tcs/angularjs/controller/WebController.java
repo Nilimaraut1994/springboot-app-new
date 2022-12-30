66cbc7ba78a2d9b6bc671baf1c5e242245f0a701
Nilimaraut1994 <79898377+Nilimaraut1994@users.noreply.github.com>
2022-12-30 06:46:12 -0800

    Update WebController.java
    java doc is created

--- src/main/java/com/tcs/angularjs/controller/WebController.java
+++ src/main/java/com/tcs/angularjs/controller/WebController.java
@@ -3,7 +3,10 @@ package com.tcs.angularjs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+
+/***
+*@auther nilima raut
+**/
 @Controller
 public class WebController {
     @RequestMapping(value="/",method = RequestMethod.GET)
